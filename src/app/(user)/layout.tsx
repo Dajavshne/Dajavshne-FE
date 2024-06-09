afa2c3c4cfd0aa7daf4df29bffc5a9de4c9ee7e8
NIK0LADZE <giorgi.nikoladze@scandiweb.com>
2024-06-09 12:22:36 +0400

    Load car tyre wash image with import

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { ReactNode } from 'react';
 
+import CarTyreWash from '@/app/(user)/images/car-tyre-wash.jpeg';
+
 import Wrapper from './wrapper';
 
 import '../globals.css';
@@ -16,10 +18,11 @@ export default function UserLayout({
       <div className="flex min-h-screen w-screen">
         <div className="relative hidden w-3/5 lg:block">
           <Image
-            src={'/images/car-tyre-wash.jpeg'}
+            src={CarTyreWash}
             width={1440}
             height={2160}
             alt="Background Image"
+            placeholder="blur"
             className="absolute -z-10 size-full object-cover"
           />
           <div className="flex size-full items-center justify-center bg-black/30">
